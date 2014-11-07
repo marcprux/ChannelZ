f0ec639cdf9eea36d7c483b300345fa9188bba21
Marc Prud'hommeaux <mwp1@cornell.edu>
2014-11-07 16:00:02 -0500

    Initial Commit

---
+++ SwiftFlow/SwiftFlow.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFlow.h
+//  SwiftFlow
+//
+//  Created by Marc Prud'hommeaux on 11/7/14.
+//  Copyright (c) 2014 glimpse.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFlow.
+FOUNDATION_EXPORT double SwiftFlowVersionNumber;
+
+//! Project version string for SwiftFlow.
+FOUNDATION_EXPORT const unsigned char SwiftFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFlow/PublicHeader.h>
+
+
