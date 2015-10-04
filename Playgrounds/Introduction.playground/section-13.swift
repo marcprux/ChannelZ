6efdb9654152360965f2b9075314276ea52eec54
Marc Prud'hommeaux <mwp1@cornell.edu>
2015-10-04 17:25:48 -0400

    Checkpoint

---
+++ Playgrounds/Introduction.playground/section-13.swift
@@ -0,0 +1,18 @@
+class ObjcIntClass : NSObject {
+    dynamic var intField: Int = 0
+}
+
+struct SwiftStringClass {
+    let stringChannel = ∞("")∞
+}
+
+let ojic = ObjcIntClass()
+let swsc = SwiftStringClass()
+
+(ojic∞ojic.intField).map({ "\($0)" }) <=∞=> (swsc.stringChannel).map({ $0.toInt() ?? 0 })
+
+ojic.intField += 55
+swsc.stringChannel.source.value // will be "55"
+
+swsc.stringChannel.source.value = "89"
+ojic.intField // will be 89
---
+++ Playgrounds/Introduction.playground/section-15.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+let button = UIButton()
+button.controlz() ∞> { (event: UIEvent) in println("Tapped Button!") }
