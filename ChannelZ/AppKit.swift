b27c1edb5fe732201c0f7dd9bf6be3c0f28b649d
Marc Prud'hommeaux <mwp1@cornell.edu>
2016-03-27 22:51:40 -0400

    Checkpoint

--- ChannelZ/AppKit.swift
+++ ChannelZ/AppKit.swift
@@ -14,13 +14,17 @@ import Foundation // workaround for compilation bug when compiling on iOS: «@ob
 #if os(OSX)
 import AppKit
 
+public protocol ChannelController: class, NSObjectProtocol, StateSource, StateSink {
+    associatedtype ContentType
+    var value: ContentType { get set }
+}
 
 /// An NSObject controller that is compatible with a StateSource and StateSink for storing and retrieving `NSObject` values from bindings
-extension NSObjectController : StateSource, StateSink {
-    public typealias T = AnyObject? // it would be nice if this were generic, but @objc forbids it
-    public typealias State = (old: T?, new: T)
+extension NSObjectController : ChannelController {
+    public typealias ContentType = AnyObject? // it would be nice if this were generic, but @objc forbids it
+    public typealias State = (old: ContentType?, new: ContentType)
 
-    public var value : T {
+    public var value : ContentType {
         get {
             return self.content
         }
@@ -30,7 +34,7 @@ extension NSObjectController : StateSource, StateSink {
         }
     }
 
-    public func put(value: T) {
+    public func put(value: ContentType) {
         self.content = value
     }
 
@@ -41,7 +45,7 @@ extension NSObjectController : StateSource, StateSink {
     /// Creates a channel for the given controller path, accounting for the `NSObjectController` limitation that
     /// change valus are not provided with KVO observation
     public func channelZControllerPath(keyPath: String)->Channel<NSObjectController, State> {
-        let kvt: KeyValueTarget<T> = KeyValueTarget(target: self, initialValue: nil, keyPath: keyPath)
+        let kvt: KeyValueTarget<ContentType> = KeyValueTarget(target: self, initialValue: nil, keyPath: keyPath)
         let channel = KeyValueOptionalSource(target: kvt).channelZState()
         // KVO on an object controlled drops the value: “Important: The Cocoa bindings controller classes do not provide change values when sending key-value observing notifications to observers. It is the developer’s responsibility to query the controller to determine the new values.”
         let resourced = channel.resource({ [unowned self] _ in self })
@@ -65,6 +69,12 @@ extension NSControl { // : KeyValueChannelSupplementing {
 
 
         return Channel<ActionTarget, Void>(source: target, reception: target.receivers.addReceipt)
+    }
+
+    /// Creates a binding to an intermediate NSObjectController with the given options and returns the bound channel
+    public func channelZBinding(binding: String = NSValueBinding, controller: NSObjectController = NSObjectController(content: nil), options: [String : AnyObject] = [:]) -> Channel<NSObjectController, NSObjectController.State> {
+        bind(binding, toObject: controller, withKeyPath: "content", options: options)
+        return controller.channelZState()
     }
 
     public func supplementKeyValueChannel(forKeyPath: String, receiver: (AnyObject?)->()) -> (()->())? {
