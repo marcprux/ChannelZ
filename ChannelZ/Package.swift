277dcdbf888909e1b96789bf43cbf7311058a1fc
Marc Prud'hommeaux <marc@prux.org>
2017-09-24 16:36:51 -0400

    Swift 4 fixes

---
+++ ChannelZ/Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "ChannelZ",
+    products: [
+        .library(
+            name: "ChannelZ",
+            targets: ["ChannelZ"]),
+        ],
+    targets: [
+        .target(
+            name: "ChannelZ",
+        .testTarget(
+            name: "ChannelZTests",
+        ]
+)
